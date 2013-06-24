b0107a2b9e8f76bebb9c6c30bb10754ef9384a61
Dennis Ideler <ideler.dennis@gmail.com>
2013-06-24 18:53:07 -0400

    Refactor ch6 exercises

--- 3773-adv-obj-oriented-app-dev-w-c++/programs/e6/e6.2.cpp
+++ 3773-adv-obj-oriented-app-dev-w-c++/programs/e6/e6.2.cpp
@@ -1,29 +1,28 @@
+// Tomasz Muldner, September 2001
+// A program for the book:
+//  "C++ Programming with Design Patterns Revealed", published by Addison-Wesley
+// Example 6.2
+// File: e6.2.intstack-d.cpp 
+//
+// Simple test program
 
-  // Tomasz Muldner, September 2001
-  // A program for the book:
-  //  "C++ Programming with Design Patterns Revealed", published by Addison-Wesley, 2002
-  // Example 6.2
-  // File: e6.2.intstack-d.cpp 
-  // Simple test program
 #include "e6.2.der-intstack.h"
 #include <iostream>
 using namespace std;
 
-int main() {
-  try {
+int main()
+{
+  try
+  {
     DerIntStack s(1);
-    cout << "new stack is " << (s.empty() ? "" : "not ") << " empty" << endl;
+    cout << "new stack is " << (s.empty() ? "" : "not ") << "empty" << endl;
     s.push(20);
-    cout << "after push stack is " << (s.full() ? "" : "not ") << " full" << endl;  
+    cout << "after push, stack is " << (s.full() ? "" : "not ") << "full" << endl;  
     cout << "popping : " << s.pop() << endl;
-    cout << "now stack is " << (s.empty() ? "" : "not ") << " empty" << endl;   
+    cout << "now stack is " << (s.empty() ? "" : "not ") << "empty" << endl;   
     s.push(33);
-    s.push(2);
+    s.push(2); // throws exception
   }
-  catch(const bad_alloc&) {
-    cout << "run out of memory" << endl;
-  }
-  catch(const logic_error& e) {
-    cout << e.what() << endl;
-  }
+  catch(const bad_alloc&) { cout << "ran out of memory" << endl; }
+  catch(const logic_error& e) { cout << e.what() << endl; }
 }
