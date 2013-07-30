16eeef5b3a04fd4102be8635cb476bb03536752a
Dennis Ideler <ideler.dennis@gmail.com>
2013-07-30 05:17:31 -0400

    Add comments. Add back(). Disallow copy and assign.

--- 3773-adv-obj-oriented-app-dev-w-c++/assignments/a5/7.11-list.h
+++ 3773-adv-obj-oriented-app-dev-w-c++/assignments/a5/7.11-list.h
@@ -2,8 +2,13 @@
  * COMP 3773 - Assignment 5 - Exercise 7.11
  * From the book "C++ Programming with Design Patterns Revealed"
  *
- * Interface (definition) for the template class List.
- * A simple list, based on the one from exercise 7.5 / example 7.1. 
+ * Interface (definitions) for the template classes List & ListIterator and
+ * template struct Link.
+ * 
+ * The list and iterator are very basic and are loosely based on example 7.1.
+ * Caution, there is no exception handling.
+ * Undefined behaviour will occur if an invalid position or range is specified.
+ * If you need a list for production, use or extend std::list instead.
  *
  * @author Dennis Ideler <ideler.dennis@gmail.com>
  * @student# 100122809
@@ -14,38 +19,38 @@
 #ifndef LIST_H_
 #define LIST_H_
 
+#define DISALLOW_COPY_AND_ASSIGN(List) \
+  List(const List<T>&);                \
+  List<T> &operator=(const List<T>&);
+
 #include <cstddef> // For size_t.
 
 namespace mylist
 {
 
-// Forward declarations (can be avoided if member classes made public).
-//struct Link;
 template <typename T>
-class ListIterator;
+class ListIterator; // Forward declaration.
 
 template <typename T>
-struct Link
+struct Link // A list is composed of many links, each holds an element.
 {
   Link(const T &t, Link *l) : value(t), next(l) {}
   T value;
   Link *next;
 };
 
-template <typename T> // Note: `class T` can also be used.
+template <typename T> // Note: <class T> can also be used.
 class List
 {
  public:
   List() : head_(nullptr), size_(0) {}
-  //List(const List<T> &other);
-  //List<T>& operator=(const List<T> &other);
+
   ~List() { while (size_ > 0) pop_front(); }
 
-  // Element access:
+  // Element access (modifiers and queries):
 
-  // Overloaded subscript operator [] returns the i-th element in the container.
-  // Caution, undefined behaviour if an invalid position or range is specified.
-  T& operator[](int position)
+  // Access i-th element.
+  T& operator[](int position) // Note: [] is called the subscript operator.
   {
     Link<T> *temp = head_;
     while (position --> 0)
@@ -53,6 +58,7 @@ class List
     return temp->value;
   }
 
+  // Access i-th element.
   const T& operator[](int position) const
   {
     Link<T> *temp = head_;
@@ -61,12 +67,29 @@ class List
     return temp->value;
   }
 
+  // Access first element.
   T& front() { return head_->value; }
   
+  // Access first element.
   const T& front() const { return head_->value; }
 
-  //T& back(); // TODO try list.end() - 1;
-  //const T& back() const;
+  // Access last element.
+  T& back()
+  {
+    Link<T> *temp = head_;
+    for (size_t i = 1; i < size_; ++i)
+      temp = temp->next;
+    return temp->value;
+  }
+
+  // Access last element.
+  const T& back() const
+  {
+    Link<T> *temp = head_;
+    for (size_t i = 1; i < size_; ++i)
+      temp = temp->next;
+    return temp->value;
+  }
   
   // Modifiers:
 
@@ -89,46 +112,40 @@ class List
 
   // Capacity:
 
-  bool empty() const
-  {
-    return size_ == 0;
-  }
+  // Test whether container is empty.
+  bool empty() const { return size_ == 0; }
 
-  int size() const
-  {
-    return size_;
-  }
+  // Return size.
+  int size() const { return size_; }
 
   // Iterators:
 
-  ListIterator<T> begin()
-  {
-    return ListIterator<T>(head_, *this);
-  }
+  // Return iterator to beginning.
+  ListIterator<T> begin() { return ListIterator<T>(head_, *this); }
 
-  ListIterator<T> end()
-  {
-    return ListIterator<T>(nullptr, *this);
-  }
+  // Return iterator to end.
+  ListIterator<T> end() { return ListIterator<T>(nullptr, *this); }
 
  private:
+  DISALLOW_COPY_AND_ASSIGN(List);
   Link<T> *head_; // The first link in the list.
   size_t size_;
 };
 
 template <typename T>
-class ListIterator
+class ListIterator // TODO: More overloaded arithmetic operators would be nice.
 {
  public:
   ListIterator(Link<T> *t, List<T> &l) : current_(t), list_(l) {}
 
-  ListIterator& operator++() // Prefix.
+  // Prefix (pre-increment). Advances the iterator by one position.
+  ListIterator& operator++()
   {
     current_ = current_->next;
     return *this;
   }
- 
-//  ListIterator& operator++(int) // Postfix.
+  
+//  ListIterator& operator++(int) // Postfix, implemented as helper instead.
 //  {
 //    ListIterator temp(*this);
 //    ++(*this);
@@ -155,12 +172,13 @@ class ListIterator
 
   template <typename U>
   friend bool operator==(const ListIterator<U>&, const ListIterator<U>&);
+
   friend class List<T>;
 };
 
 // Helpers
 
-// Postfix, post-increments the iterator. Dummy int needed to differentiate.
+// Postfix (post-increment). Dummy int needed to differentiate.
 // Preference in the book is to have this as a helper because it returns a new
 // object. My preference is to have it as a member, but I still kept it as
 // a helper to demonstrate how to befriend it (see ListIterator class).
