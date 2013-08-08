a725a0bbfb9452fd9cf2dbdfc91ed791007e6c0f
Dennis Ideler <ideler.dennis@gmail.com>
2013-08-08 02:53:51 -0400

    Clean up exercise 8.14

--- 3773-adv-obj-oriented-app-dev-w-c++/programs/e8/e8.14.cpp
+++ 3773-adv-obj-oriented-app-dev-w-c++/programs/e8/e8.14.cpp
@@ -1,33 +1,44 @@
-  // Tomasz Muldner, September 2001
-  // A program for the book:
-  //  "C++ Programming with Design Patterns Revealed", published by Addison-Wesley, 2002
-  // Exercise 8.14
+// Tomasz Muldner, September 2001
+// A program for the book:
+//  "C++ Programming with Design Patterns Revealed", published by Addison-Wesley
+// Exercise 8.14
+
 #include <iostream>
-using namespace std;
 #include <list>
+using namespace std;
+
+// Template function concatenates two lists.
 template <typename T>
-list<T> concatenate(const list<T>& one, const list<T>& two) {
+list<T> concatenate(const list<T> &one, const list<T> &two)
+{
   list<T> head(one.begin(), one.end());
   list<T> temp(two.begin(), two.end());
-  head.splice(head.end(), temp);
+  head.splice(head.end(), temp); // Transfer temp list to end of head.
   return head;
-} 
-  int main() {
-    list<int> one;
-    list<int> two;
-    for(int i = 0; i < 10; ++i) {
-      one.insert(one.end(), i);
-      two.insert(two.end(), 10-i);
-   }
-    for(list<int>::iterator i = one.begin(); i != one.end(); ++i)
-     cout << *i << " ";
-   cout << endl;
- for(list<int>::iterator i = two.begin(); i != two.end(); ++i)
-     cout << *i << " ";
-   cout << endl;
-   list<int> three = concatenate(one, two);
-   for(list<int>::iterator i = three.begin(); i != three.end(); ++i)
-     cout << *i << " ";
-   cout << endl;
-    
+}
+
+int main()
+{
+  list<int> one;
+  list<int> two;
+
+  for (int i = 0; i < 10; ++i)
+  {
+    one.insert(one.end(), i); // push_back() also works.
+    two.insert(two.end(), 10 - i);
   }
+
+  for (list<int>::iterator i = one.begin(); i != one.end(); ++i)
+    cout << *i << " ";
+  cout << endl;
+
+  for (list<int>::iterator i = two.begin(); i != two.end(); ++i)
+    cout << *i << " ";
+  cout << endl;
+
+  list<int> three = concatenate(one, two);
+
+  for (list<int>::iterator i = three.begin(); i != three.end(); ++i)
+    cout << *i << " ";
+  cout << endl;
+}
